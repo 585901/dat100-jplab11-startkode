0d8934d64a57ff8a005bb91db72e99432135dc7d
585901 <111574775+585901@users.noreply.github.com>
2022-10-25 11:25:53 +0200

    Update Blogg.java

--- src/no/hvl/dat100/jplab11/oppgave3/Blogg.java
+++ src/no/hvl/dat100/jplab11/oppgave3/Blogg.java
@@ -3,70 +3,148 @@ package no.hvl.dat100.jplab11.oppgave3;
 import no.hvl.dat100.jplab11.common.TODO;
 import no.hvl.dat100.jplab11.oppgave1.*;
 
-public class Blogg {
+public class Blogg {  
 
-	// TODO: objektvariable 
+// A) Klassen Blogg skal lagre Innlegg-objektene sammenhengene fra starten av tabellen 
+//og ha følgende objektvariable: 
+//innleggtabell som er en referanse tabell av Innlegg-objekt.
+//nesteledig som angir antall Innlegg-objekt som er lagret i tabellen og dermed neste 
+//ledige posisjon i tabellen
 
+	private Innlegg[] innleggtabell; 
+	private int nesteledig; 
+	
+// B) Implementer en standard konstruktør: public Blogg()
+//der startstørrelsen på tabellen blir satt til 20 samt en konstruktør:
+//public Blogg(int lengde)
+//der parameteren angir lengden angir startstørrelsen på tabellen.
+	
 	public Blogg() {
-		throw new UnsupportedOperationException(TODO.constructor("Blogg"));
+		this.innleggtabell = new Innlegg[20]; 
 	}
 
 	public Blogg(int lengde) {
-		throw new UnsupportedOperationException(TODO.constructor("Blogg"));
+		this.innleggtabell = new Innlegg[lengde]; 
+		this.nesteledig = 0; 
 	}
 
+// C) En metode public int getAntall() som returnerer antall innlegg-objekt som 
+//	aktuelt er lagret i tabellen.
+	
 	public int getAntall() {
-		throw new UnsupportedOperationException(TODO.method());
+		return this.nesteledig; 
 	}
 	
+// D) En metode public Innlegg[] getSamling() som returnerer en peker til 
+//	tabellen av innlegg-objekt.
+	
 	public Innlegg[] getSamling() {
-		throw new UnsupportedOperationException(TODO.method());
+		return this.innleggtabell; 
 
 	}
+	
+// E) En metode public int finnInnlegg(Innlegg innlegg) som returnerer indeks 
+//(posisjon) i tabellen for et innlegg i samlingen med samme id som parameteren 
+//innlegg (om en slik finnes) og -1 ellers. Bruk erLik-metoden for Innlegg-objekt 
+//i implementasjonen. 
 	
 	public int finnInnlegg(Innlegg innlegg) {
 
-		throw new UnsupportedOperationException(TODO.method());
+		for (int i = 0; i <innleggtabell.length; i++) {
+
+			if(innleggtabell[i] == null){
+				return -1;
+			}
+			if(innleggtabell[i].erLik(innlegg)){
+				return i;
+
+			}
+
+		}
+		return -1;
 	}
 
+// F) En metode public boolean finnes(Innlegg innlegg) som returnerer true om der finnes 
+//et innlegg i samlingen med samme id som innlegget gitt ved parameteren innlegg.
+	
 	public boolean finnes(Innlegg innlegg) {
-		throw new UnsupportedOperationException(TODO.method());
+		for (int i = 0; i <innleggtabell.length; i++) {
+			if (innleggtabell[i] == null){
+				return false;
+			}
+			if(innlegg.getId() == innleggtabell[i].getId()){
+				return true;
+			}
+
+		}
+		return false;
 	}
 
+// G) En metode public boolean ledigPlass() som returnerer true om der er ledig 
+//plass i samlingen og false ellers.
+	
 	public boolean ledigPlass() {
-		throw new UnsupportedOperationException(TODO.method());
+		if(nesteledig < innleggtabell.length){
+			return true;
+		}
+		return false;
 
 	}
+	
+// H) En metode public boolean leggTil(Innlegg innlegg) som legger innlegg inn i 
+//tabellen. Dersom der ikke finnes et innlegg i tabellen med samme id som innlegg 
+//skal metoden legge til innlegget på neste ledige plass i tabellen. Ellers skal 
+//ikke metoden legge inn innlegg i tabellen. Metoden skal returnere true om 
+//innlegget blev lagt til og false ellers.
 	
 	public boolean leggTil(Innlegg innlegg) {
 
-		throw new UnsupportedOperationException(TODO.method());
+		if(finnes(innlegg)){
+			return false;
+		}else {
+			innleggtabell[nesteledig] = innlegg;
+			nesteledig++;
+			return true;
+			}
 	}
 	
+// I) En metode public String toString() som returnerer data i tabellen som en streng der 
+//første linje i strengen angir antall Innlegg-objekt i tabellen, eks., en tabell med to innlegg 
+//(et tekst-innlegg og et bilde-innlegg): 
+//2\nTEKST\n1\nOle Olsen\n23-10\n0\nen tekst\nBILDE\n2\nOline Olsen\n24-10\n0\net bilde\nhttp://www.picture.com/oo.jpg\n
+	
 	public String toString() {
-		throw new UnsupportedOperationException(TODO.method());
+		String tabelltekst = "" +  nesteledig + "\n";
+
+		for (int i = 0; i <innleggtabell.length; i++) {
+
+			tabelltekst += innleggtabell[i].toString();
+
+		}
+
+		return tabelltekst;
 	}
 
 	// valgfrie oppgaver nedenfor
 	
-	public void utvid() {
-		throw new UnsupportedOperationException(TODO.method());
-	}
+//	public void utvid() {
+//		throw new UnsupportedOperationException(TODO.method());
+//	}
 	
-	public boolean leggTilUtvid(Innlegg innlegg) {
+//	public boolean leggTilUtvid(Innlegg innlegg) {
 
-		throw new UnsupportedOperationException(TODO.method());
+//		throw new UnsupportedOperationException(TODO.method());
 		
-	}
+//	}
 	
-	public boolean slett(Innlegg innlegg) {
+//	public boolean slett(Innlegg innlegg) {
 		
-		throw new UnsupportedOperationException(TODO.method());
-	}
+//		throw new UnsupportedOperationException(TODO.method());
+//	}
 	
-	public int[] search(String keyword) {
+//	public int[] search(String keyword) {
 		
-		throw new UnsupportedOperationException(TODO.method());
+//		throw new UnsupportedOperationException(TODO.method());
 
-	}
+//	}
 }
